518ba18c9efc2db80981e386f332ae5d5624561f
Patrick Flanagan <plpflanagan@gmail.com>
2018-11-12 14:56:33 -0800

    add anchorage for future cleanup

--- Example/Pods/Target Support Files/Pods-UNUMCanvas_Example/Pods-UNUMCanvas_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-UNUMCanvas_Example/Pods-UNUMCanvas_Example-frameworks.sh
@@ -143,9 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UNUMCanvas/UNUMCanvas.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/UNUMCanvas/UNUMCanvas.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
