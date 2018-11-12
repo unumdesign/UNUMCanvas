24af8185249bd17d4dcf7068addf0b236e3c8f04
Patrick Flanagan <plpflanagan@gmail.com>
2018-11-12 13:27:19 -0800

    change other examples to constraints

--- Example/UNUMCanvas/ExampleSingleCanvasViewController.swift
+++ Example/UNUMCanvas/ExampleSingleCanvasViewController.swift
@@ -23,14 +23,23 @@ final class ExampleSingleCanvasViewController: UIViewController {
         view.backgroundColor = .white
         
         // Add some views that should be interactable.
-        interactableView1 = UIView(frame: CGRect(x: 100, y: 400, width: 100, height: 100))
-        interactableView1.backgroundColor = .orange
+        interactableView1 = UIView()//UIView(frame: CGRect(x: 100, y: 400, width: 100, height: 100))
         view.addSubview(interactableView1)
+        interactableView1.translatesAutoresizingMaskIntoConstraints = false
+        interactableView1.topAnchor.constraint(equalTo: interactableView1.superview!.topAnchor, constant: 400).isActive = true
+        interactableView1.leadingAnchor.constraint(equalTo: interactableView1.superview!.leadingAnchor, constant: 100).isActive = true
+        interactableView1.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        interactableView1.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        interactableView1.backgroundColor = .orange
         
-        interactableView2 = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
+        interactableView2 = UIView()//UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
         interactableView2.backgroundColor = .green
         view.addSubview(interactableView2)
-        
+        interactableView2.translatesAutoresizingMaskIntoConstraints = false
+        interactableView2.topAnchor.constraint(equalTo: interactableView1.superview!.topAnchor, constant: 100).isActive = true
+        interactableView2.leadingAnchor.constraint(equalTo: interactableView1.superview!.leadingAnchor, constant: 100).isActive = true
+        interactableView2.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        interactableView2.heightAnchor.constraint(equalToConstant: 100).isActive = true
         
         canvasRegion.interactableViews.append(contentsOf: [interactableView1, interactableView2])
         canvasRegion.canvasViews = [view]
