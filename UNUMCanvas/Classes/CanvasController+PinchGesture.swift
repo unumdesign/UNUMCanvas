c42c28b33aaed357c383eab7966cb593957cf86e
Patrick Flanagan <plpflanagan@gmail.com>
2018-11-29 12:19:19 -0800

    fix scaling for width-bound-to-height views

--- UNUMCanvas/Classes/CanvasController+PinchGesture.swift
+++ UNUMCanvas/Classes/CanvasController+PinchGesture.swift
@@ -15,43 +15,78 @@ extension CanvasController {
         guard let selectedView = selectedView else {
             return
         }
-        
-        var heightDifference: CGFloat = 0
-        var widthDifference: CGFloat = 0
-        
-        // increase width and height according to scale
-        if let widthConstraint = selectedView.widthConstraint {
-            let previousWidth = widthConstraint.constant
-            widthConstraint.constant = widthConstraint.constant * sender.scale
-            widthDifference = widthConstraint.constant - previousWidth
-        }
-        
-        
-        if let heightConstraint = selectedView.heightConstraint {
+
+        if
+            selectedView.widthIsBoundToHeight,
+            let heightConstraint = selectedView.heightConstraint
+        {
             let previousHeight = heightConstraint.constant
-            heightConstraint.constant = heightConstraint.constant * sender.scale
-            
+            let newHeight = heightConstraint.constant * sender.scale
+
+            heightConstraint.constant = newHeight
+
+            var ratio: CGFloat = 1.0
             if
                 let selectedImageView = selectedView as? UIImageView,
                 let image = selectedImageView.image
             {
-                let ratio = image.size.height / image.size.width
-                heightDifference = widthDifference * ratio
+                ratio = image.size.width / image.size.height
             }
-            else {
-                heightDifference = heightConstraint.constant - previousHeight
+
+            let heightDifference = newHeight - previousHeight
+            let widthDifference = heightDifference * ratio
+
+            // adjust leading and top anchors to keep view centered
+            if let leadingConstraint = selectedView.leadingConstraint {
+                leadingConstraint.constant = leadingConstraint.constant - widthDifference / 2
+            }
+            if let topConstraint = selectedView.topConstraint {
+                topConstraint.constant = topConstraint.constant - heightDifference / 2
             }
         }
-        
-        // adjust leading and top anchors to keep view centered
-        if let leadingConstraint = selectedView.leadingConstraint {
-            leadingConstraint.constant = leadingConstraint.constant - widthDifference / 2
+        else if
+            selectedView.heightIsBoundToWidth,
+            let widthConstraint = selectedView.widthConstraint
+        {
+            print("heightIsboutntoWidth")
+        }
+        else {
+            print("neither")
         }
         
-        if let topConstraint = selectedView.topConstraint {
-            topConstraint.constant = topConstraint.constant - heightDifference / 2
-        }
-        
+//        // increase width and height according to scale
+//        if let widthConstraint = selectedView.widthConstraint {
+//            let previousWidth = widthConstraint.constant
+//            widthConstraint.constant = widthConstraint.constant * sender.scale
+//            widthDifference = widthConstraint.constant - previousWidth
+//        }
+//
+//
+//        if let heightConstraint = selectedView.heightConstraint {
+//            let previousHeight = heightConstraint.constant
+//            heightConstraint.constant = heightConstraint.constant * sender.scale
+//
+//            if
+//                let selectedImageView = selectedView as? UIImageView,
+//                let image = selectedImageView.image
+//            {
+//                let ratio = image.size.height / image.size.width
+//                heightDifference = widthDifference * ratio
+//            }
+//            else {
+//                heightDifference = heightConstraint.constant - previousHeight
+//            }
+//        }
+//
+//        // adjust leading and top anchors to keep view centered
+//        if let leadingConstraint = selectedView.leadingConstraint {
+//            leadingConstraint.constant = leadingConstraint.constant - widthDifference / 2
+//        }
+//
+//        if let topConstraint = selectedView.topConstraint {
+//            topConstraint.constant = topConstraint.constant - heightDifference / 2
+//        }
+
         // reset scale after applying in order to keep scaling linear rather than exponential
         sender.scale = 1.0
     }
