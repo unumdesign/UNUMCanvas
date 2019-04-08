799507051c785f15b870dfde7021a3a003a94548
Patrick Flanagan <patrick@unum.la>
2019-04-08 14:25:12 -0700

    Make VideoPlayerView have an image of start of video
    This simplifies having a snapshot for BirdsEyeView since the video would
    never actually show up in the snapshot. Make sure the video is above the
    image so that the image is hidden from the user and they are only aware
    of the video.

--- UNUMCanvas/Classes/AVPlayerView.swift
+++ UNUMCanvas/Classes/AVPlayerView.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import AVKit
+import Anchorage
 
 public enum PlayerViewFactory {
     public static func makePlayerView(with avAsset: AVAsset) -> AVPlayerView {
@@ -26,6 +27,57 @@ public enum PlayerViewFactory {
 
 public class AVPlayerView: UIView {
 
+    private var videoView: VideoView?
+
+    public var videoPlayer: AVPlayer {
+        guard let videoView = videoView else {
+            assertionFailure("Player Layer should be set at init.")
+            return AVPlayer(playerItem: nil)
+        }
+        return videoView.videoPlayer
+    }
+
+    public init(player: AVPlayer) {
+        super.init(frame: .zero)
+
+        let image = getImage(from: player)
+        let imageView = UIImageView(image: image)
+        addSubview(imageView)
+        imageView.edgeAnchors == edgeAnchors
+
+        let videoView = VideoView(player: player)
+        addSubview(videoView)
+        videoView.edgeAnchors == edgeAnchors
+        self.videoView = videoView
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    private func getImage(from player: AVPlayer) -> UIImage? {
+        guard let asset = player.currentItem?.asset else {
+            return nil
+        }
+
+        let assetIG = AVAssetImageGenerator(asset: asset)
+        assetIG.appliesPreferredTrackTransform = true
+        assetIG.apertureMode = AVAssetImageGenerator.ApertureMode.encodedPixels
+
+        let cmTime = CMTime(seconds: 0, preferredTimescale: 60)
+        let thumbnailImageRef: CGImage
+        do {
+            thumbnailImageRef = try assetIG.copyCGImage(at: cmTime, actualTime: nil)
+        } catch let error {
+            print("Error: \(error)")
+            return nil
+        }
+
+        return UIImage(cgImage: thumbnailImageRef)
+    }
+}
+
+private class VideoView: UIView {
     public var videoPlayer: AVPlayer {
         guard
             let playerLayer = layer as? AVPlayerLayer,
@@ -54,4 +106,3 @@ public class AVPlayerView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 }
-
